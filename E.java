8d51d28c17778a13a8471818da42fc4f238eed22
denak <denak1607@gmail.com>
2026-01-29 11:00:11 +0300

    Implented part e from the assignment.
    Wrote a method for finding the sum of odd or even numbered integers in an array.

---
+++ E.java
@@ -0,0 +1,23 @@
+class E {
+        /**
+        ikinci parametre true ise tek sayılar, false ise çift sayıları topluyo
+         */
+        public static int sumEvenOdd (int[] arr, boolean isOdd) {
+            int result = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (isOdd) {
+                    if (i%2 == 0) {
+                        result += arr[i];
+                    }
+                }
+                else {
+                    if (i%2 == 1) {
+                        result += arr[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
