784b58f920a057ca7b4d14b97bccd5367fb948d8
Eren Kozan <erenkozan2007@gmail.com>
2026-01-29 11:43:36 +0300

    Update A.java

--- A.java
+++ A.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 public class A {
     public int[] generateArray(int size){
-        int[] randomArray = new int[10];
+        int[] randomArray = new int[size];
         Random rnd = new Random();
-        for(int i = 0; i<randomArray.length; i++  ){
+        for(int i = 0; i < randomArray.length; i++){
             randomArray[i] = rnd.nextInt(101);
         }
         return randomArray;
