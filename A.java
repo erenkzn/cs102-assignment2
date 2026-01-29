d9d7470b984ca0b0232ea84db6f5c7ed21154800
Eren Kozan <erenkozan2007@gmail.com>
2026-01-29 11:42:12 +0300

    updated

--- A.java
+++ A.java
@@ -1,14 +1,11 @@
 import java.util.Random;
-import java.util.Arrays;
 public class A {
-    public static void main(String []args){
-
+    public int[] generateArray(int size){
         int[] randomArray = new int[10];
         Random rnd = new Random();
         for(int i = 0; i<randomArray.length; i++  ){
             randomArray[i] = rnd.nextInt(101);
         }
-        System.out.print(Arrays.toString(randomArray));
+        return randomArray;
     }
-
 }
